f08909cf520829707530ff1aa74a12286754fb0f
Abdullah <l201326@lhr.nu.edu.pk>
2025-03-04 22:06:47 +0500

    modified

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
